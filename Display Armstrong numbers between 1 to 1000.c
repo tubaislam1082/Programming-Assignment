ee50c46910dc715fc9ed4ae5a620082024dd3bef
Tubaislam <sanjidaislam1089@gmail.com>
2026-06-30 14:18:34 +0600

    Update Display Armstrong numbers between 1 to 1000.c

--- Display Armstrong numbers between 1 to 1000.c
+++ Display Armstrong numbers between 1 to 1000.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,n,r,sum,temp;
-    scanf("%d%d",&a,&b);
-    for(n=a;n<=b;n++)
+    int n,r,sum,temp;
+    for(n=1;n<=1000;n++)
     {
     temp=n;
     sum=0;
@@ -13,8 +12,8 @@ int main()
         sum=sum+r*r*r;
         temp=temp/10;
         }
-        if(sum==0)
-            printf("%d ",n);
+        if(sum==n)
+            printf("%d",n);
         }
         return 0;
 }
